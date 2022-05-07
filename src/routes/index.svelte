f193c93b6cd699de59ce69280e4f6e44e28bc6ca
Matt Gleich <git@mattglei.ch>
2022-05-06 23:46:48 -0400

    feat: show results
    Signed-off-by: Matt Gleich <git@mattglei.ch>

---
+++ src/routes/index.svelte
@@ -0,0 +1,18 @@
+<script>
+    let games;
+
+    fetch('https://ghs-games.api.mattglei.ch/current-week', {mode: 'cors'})
+        .then((r) => r.json())
+        .then((r) => {
+            games = r
+        })
+</script>
+
+<main>
+    {#if games == undefined}
+        <p>Loading</p>
+    {:else}
+        {games["data"][0]["name"]}
+    {/if}
+</main>
+
